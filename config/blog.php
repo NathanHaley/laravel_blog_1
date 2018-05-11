34b6356e513fd9b9d2f2350f4ec5d9d0bbde77f3
Nathan Haley <n3274@nathanhaley.com>
2018-05-11 00:35:08 -0500

    Add config file

--- config/blog.php
+++ config/blog.php
@@ -4,4 +4,14 @@ return [
     'administrators' => [
         'a@a.com'
     ],
+    'permissions' => [
+        'table_names' => [
+            'permissions' => 'permissions',
+            'roles' => 'roles',
+            'model_has_permissions' => 'model_has_permissions',
+            'model_has_roles' => 'model_has_roles',
+            'role_has_permissions' => 'role_has_permissions'
+
+        ]
+    ]
 ];
